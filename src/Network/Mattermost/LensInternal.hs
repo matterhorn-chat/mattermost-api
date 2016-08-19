a655ea040412cb9a72ff1ccea8ec4bc7483487c5
Getty Ritter <gdritter@galois.com>
2016-08-19 00:27:08 -0700

    Removed unneeded code from LensInternal.
    (All we need are the Lens' alias and the makeLens function!)

--- src/Network/Mattermost/LensInternal.hs
+++ src/Network/Mattermost/LensInternal.hs
@@ -3,20 +3,10 @@
 
 module Network.Mattermost.LensInternal (Lens', makeLens) where
 
-
--- We need these to write get and set
-newtype C a b = C { fromC :: a } deriving (Functor)
-newtype I a = I { fromI :: a } deriving (Functor)
-
 -- This is the same type alias as in @Control.Lens@, and so can be used
 -- anywhere lenses are needed.
 type Lens' a b = forall f. Functor f => (b -> f b) -> (a -> f a)
 
-get :: Lens' a b -> a -> b
-get lens a = fromC (lens C a)
-
-set :: Lens' a b -> b -> a -> a
-set lens x a = fromI (lens (const I x) a)
-
+-- Create a lens by combining a getter and a setter.
 makeLens :: (a -> b) -> (b -> a -> a) -> Lens' a b
 makeLens get set f a = (`set` a) `fmap` f (get a)
