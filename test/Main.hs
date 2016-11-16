0ccca30f777f9c7cf370865206ea517db020f0b9
Jason Dagit <dagitj@gmail.com>
2016-11-16 14:32:41 -0800

    Refactor test suite

--- test/Main.hs
+++ test/Main.hs
@@ -44,13 +44,15 @@ main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testCaseSteps "MM Tests" $ \step -> do
-  step "Creating Admin account"
-  _token <- setup step
+tests = testCaseSteps "MM Tests" $ \prnt -> do
+  prnt "Creating Admin account"
+  createAdminAccount prnt
+  prnt "Logging into Admin account"
+  _token <- loginAdminAccount prnt
   return ()
 
-setup :: (String -> IO ()) -> IO Token
-setup prnt = do
+createAdminAccount :: (String -> IO ()) -> IO ()
+createAdminAccount prnt = do
   cd <- initConnectionDataInsecure (T.unpack (configHostname testConfig))
                                    (fromIntegral (configPort testConfig))
   let newAccount = UsersCreate { usersCreateEmail          = configEmail    testConfig
@@ -66,6 +68,14 @@ setup prnt = do
                     , password = configPassword testConfig
                     }
   prnt "Admin user created"
+
+loginAdminAccount :: (String -> IO ()) -> IO Token
+loginAdminAccount prnt = do
+  let login = Login { username = configUsername testConfig
+                    , password = configPassword testConfig
+                    }
+  cd <- initConnectionDataInsecure (T.unpack (configHostname testConfig))
+                                   (fromIntegral (configPort testConfig))
   (token, mmUser) <- join (hoistE <$> mmLogin cd login)
   prnt "Authenticated as admin user"
   return token
