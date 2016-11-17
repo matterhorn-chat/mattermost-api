bc360f29beb59c9fd051063f7bf831884c9d9400
Jason Dagit <dagitj@gmail.com>
2016-11-16 17:22:19 -0800

    Refactor test suite to use test groups

--- test/Main.hs
+++ test/Main.hs
@@ -20,6 +20,11 @@ import           Network.Mattermost
 import           Network.Mattermost.Logging
 import           Network.Mattermost.Util
 
+main :: IO ()
+main = defaultMain tests
+
+-- Users and other test configuration data
+
 data Config
   = Config
   { configUsername :: Text
@@ -40,19 +45,34 @@ testConfig = Config
   , configPassword = "password"
   }
 
-main :: IO ()
-main = defaultMain tests
+testUserLogin :: Login
+testUserLogin = Login
+  { username = "test-user"
+  , password = "password"
+  }
+
+-- Test groups
 
 tests :: TestTree
-tests = testCaseSteps "MM Tests" $ \prnt -> do
+tests = testGroup "Tests" [setup,unitTests]
+
+unitTests :: TestTree
+unitTests = testGroup "Units" [createNormalUserTest
+                              ,loginAsNormalUserTest
+                              ]
+
+-- Test definitions
+
+setup :: TestTree
+setup = testCaseSteps "Setup" $ \prnt -> do
   prnt "Creating connection"
   cd <- initConnectionDataInsecure (T.unpack (configHostname testConfig))
-                                    (fromIntegral (configPort testConfig))
+                                   (fromIntegral (configPort testConfig))
   -- XXX: Use something like this if you want logging (useful when debugging)
   -- let cd = cd' `withLogger` mmLoggerDebugErr
 
   prnt "Creating Admin account"
-  createAdminAccount cd prnt
+  adminUser <- createAdminAccount cd prnt
   prnt "Logging into Admin account"
   adminToken <- loginAdminAccount cd prnt
 
@@ -73,11 +93,24 @@ tests = testCaseSteps "MM Tests" $ \prnt -> do
                                            (Bool True) teamSettings)) oldConfig)
   mmSaveConfig cd adminToken newConfig
 
-  prnt "Creating normal account"
-  createTestAccount cd adminToken prnt
-
-  --
+createNormalUserTest :: TestTree
+createNormalUserTest = testCaseSteps "Creating normal account" $ \prnt -> do
+  cd <- initConnectionDataInsecure (T.unpack (configHostname testConfig))
+                                   (fromIntegral (configPort testConfig))
+  prnt "Logging into Admin account"
+  adminToken <- loginAdminAccount cd prnt
+  prnt "Creating test account"
+  testUser   <- createTestAccount cd adminToken prnt
   return ()
+
+loginAsNormalUserTest :: TestTree
+loginAsNormalUserTest = testCaseSteps "Logging to normal account" $ \prnt -> do
+  cd <- initConnectionDataInsecure (T.unpack (configHostname testConfig))
+                                   (fromIntegral (configPort testConfig))
+  userToken <- loginAccount cd testUserLogin prnt
+  return ()
+
+-- Wrapper functions used in test cases
 
 createAdminAccount :: ConnectionData -> (String -> IO ()) -> IO ()
 createAdminAccount cd prnt = do
@@ -99,21 +132,26 @@ createTestTeam cd token prnt = do
   prnt "Test team created"
   return team
 
-createTestAccount :: ConnectionData -> Token -> (String -> IO ()) -> IO ()
+createTestAccount :: ConnectionData -> Token -> (String -> IO ()) -> IO User
 createTestAccount cd token prnt = do
   let newAccount = UsersCreate { usersCreateEmail          = "test-user@example.com"
-                               , usersCreatePassword       = "password"
-                               , usersCreateUsername       = "test-user"
+                               , usersCreatePassword       = password testUserLogin
+                               , usersCreateUsername       = username testUserLogin
                                , usersCreateAllowMarketing = False
                                }
   newUser <- mmUsersCreateWithToken cd token newAccount
   prnt "Test Account created"
+  return newUser
 
 loginAdminAccount :: ConnectionData -> (String -> IO ()) -> IO Token
-loginAdminAccount cd prnt = do
-  let login = Login { username = configUsername testConfig
-                    , password = configPassword testConfig
-                    }
+loginAdminAccount cd = loginAccount cd admin
+  where
+  admin = Login { username = configUsername testConfig
+                , password = configPassword testConfig
+                }
+
+loginAccount :: ConnectionData -> Login -> (String -> IO ()) -> IO Token
+loginAccount cd login prnt = do
   (token, mmUser) <- join (hoistE <$> mmLogin cd login)
-  prnt "Authenticated as admin user"
+  prnt $ "Authenticated as " ++ T.unpack (username login)
   return token
