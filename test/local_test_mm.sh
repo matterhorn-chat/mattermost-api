7f16dd04e24092b4b1e1992eb5d0c67878541bc0
Jonathan Daugherty <cygnus@foobox.com>
2016-12-01 13:48:51 -0800

    local_test_mm: use functions to make control flow clearer

--- test/local_test_mm.sh
+++ test/local_test_mm.sh
@@ -7,6 +7,26 @@ TEST_PROGRAM=test-mm-api
 ROOT=$HERE/..
 CONTAINER=mattermost-preview
 
+function docker_installed {
+    which docker 2>/dev/null >/dev/null
+}
+
+function container_present {
+    docker ps | grep $CONTAINER >/dev/null
+}
+
+function cleanup_last_container {
+    # These first two docker commands are allowed to fail. For instance,
+    # on a first run of this script. This check ensures that if they
+    # do fail, we abort the script because the container exists AND
+    # stopping it failed somehow.
+    if container_present
+    then
+        docker stop $CONTAINER
+        docker rm   $CONTAINER
+    fi
+}
+
 # Note: [JED] You may need to change TEST_RUNNER depending on your environment.
 # As of writing this script `cabal new-build` doesn't have a `new-run` so I use
 # the following find command to locate the test-mm-api executable. YMMV.
@@ -18,21 +38,13 @@ then
     exit 1
 fi
 
-if ! which docker 2>/dev/null >/dev/null
+if ! docker_installed
 then
     echo "Error: 'docker' not found in PATH, exiting"
     exit 1
 fi
 
-# These first two docker commands are allowed to fail. For instance,
-# on a first run of this script. This check ensures that if they do
-# fail, we abort the script because the container exists AND stopping it
-# failed somehow.
-if docker ps | grep $CONTAINER >/dev/null
-then
-    docker stop $CONTAINER
-    docker rm   $CONTAINER
-fi
+cleanup_last_container
 
 # If this command fails we're in trouble.
 docker run  --name $CONTAINER -d --publish 8065:8065 mattermost/$CONTAINER
