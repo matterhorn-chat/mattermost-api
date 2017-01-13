d38518652bdf4f972b135428e5998c25cf0c7bdf
Getty Ritter <gdritter@galois.com>
2017-01-12 16:17:40 -0800

    Added Version module that contains library version string

---
+++ src/Network/Mattermost/Version.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Network.Mattermost.Version (mmApiVersion) where
+
+import Data.Version (showVersion)
+import Development.GitRev (gitBranch, gitHash)
+import Paths_mattermost_api (version)
+
+mmApiVersion :: String
+mmApiVersion
+  | $(gitHash) == ("UNKNOWN" :: String) = "mattermost-api " ++ showVersion version
+  | otherwise = "mattermost-api " ++ showVersion version ++ " (" ++
+                $(gitBranch) ++ "@" ++ $(gitHash) ++ ")"
