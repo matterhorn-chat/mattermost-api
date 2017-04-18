45f47df54859f50add8fa2e023ebb98b9d37c358
Jason Dagit <dagitj@gmail.com>
2017-04-17 21:19:48 -0700

    switch back to plain http

--- test/wait_for_mm.sh
+++ test/wait_for_mm.sh
@@ -2,7 +2,7 @@
 
 ECHO=/bin/echo
 $ECHO "Waiting for mattermost server to become available"
-while ! curl https://localhost:8065 1> /dev/null 2> /dev/null
+while ! curl http://localhost:8065 1> /dev/null 2> /dev/null
 do
   $ECHO -n "."
   sleep 1
