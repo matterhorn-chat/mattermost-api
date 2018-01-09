ffde097e67df764cd737a19ad394bc2cf86cb42b
Getty Ritter <gdritter@galois.com>
2018-01-09 13:20:40 -0800

    Move HTTP.Stream instance to newtype in different module
    This creates a type MMConn, which wraps the underlying Connection for
    the purposes of our HTTP library, and also puts it into the common
    Util module. Before, we had to import Network.Mattermost for that
    instance, which was less than ideal. This also moves much of the HTTP
    helper functions from Network.Mattermost.Endpoints into a new,
    non-exposed module, Network.Mattermost.Connection.

--- src/Network/Mattermost/Endpoints.hs
+++ src/Network/Mattermost/Endpoints.hs
@@ -3,12 +3,8 @@
 
 module Network.Mattermost.Endpoints where
 
-import           Control.Arrow (left)
-import           Control.Exception (throwIO)
 import qualified Data.Aeson as A
 import qualified Data.ByteString.Char8 as B
-import qualified Data.ByteString.Lazy.Char8 as BL
-import qualified Data.List as List
 import qualified Data.HashMap.Strict as HM
 import           Data.Sequence (Seq)
 import           Data.Text (Text)
@@ -16,172 +12,14 @@ import qualified Data.Text as T
 import           Data.Time.Clock ( UTCTime )
 import qualified Network.HTTP.Base as HTTP
 import qualified Network.HTTP.Headers as HTTP
-import qualified Network.HTTP.Stream as HTTP
-import qualified Network.URI as URI
 import           Text.Printf (printf)
 
+import Network.Mattermost.Connection
 import Network.Mattermost.Exceptions
-import Network.Mattermost ()
 import Network.Mattermost.Types
 import Network.Mattermost.Types.Config
 import Network.Mattermost.Types.Internal
-import Network.Mattermost.Util
 
-
--- | Parse a path, failing if we cannot.
-mmPath :: String -> IO URI.URI
-mmPath str =
-  noteE (URI.parseRelativeReference str)
-        (URIParseException ("mmPath: " ++ str))
-
-
--- | Parse the JSON body out of a request, failing if it isn't an
---   'application/json' response, or if the parsing failed
-jsonResponse :: A.FromJSON t => HTTP.Response_String -> IO t
-jsonResponse rsp = do
-  contentType <- mmGetHeader rsp HTTP.HdrContentType
-  assertE (contentType ~= "application/json")
-          (ContentTypeException
-            ("Expected content type 'application/json'" ++
-             " found " ++ contentType))
-
-  hoistE $ left (\s -> JSONDecodeException s (HTTP.rspBody rsp))
-                (A.eitherDecode (BL.pack (HTTP.rspBody rsp)))
-
--- | Parse the JSON body out of a request, failing if it isn't an
---   'application/json' response, or if the parsing failed
-bytestringResponse :: HTTP.Response_String -> IO B.ByteString
-bytestringResponse rsp =
-  return (B.pack (HTTP.rspBody rsp))
-
-
-noResponse :: HTTP.Response_String -> IO ()
-noResponse _ = return ()
-
-
--- | Grab a header from the response, failing if it isn't present
-mmGetHeader :: HTTP.Response_String -> HTTP.HeaderName -> IO String
-mmGetHeader rsp hdr =
-  noteE (HTTP.lookupHeader hdr (HTTP.rspHeaders rsp))
-        (HeaderNotFoundException ("mmGetHeader: " ++ show hdr))
-
--- | Parse the JSON body out of a request, failing if it isn't an
---   'application/json' response, or if the parsing failed
-mmGetJSONBody :: A.FromJSON t => String -> HTTP.Response_String -> IO (t)
-mmGetJSONBody label rsp = do
-  contentType <- mmGetHeader rsp HTTP.HdrContentType
-  assertE (contentType ~= "application/json")
-          (ContentTypeException
-            ("mmGetJSONBody: " ++ label ++ ": " ++
-             "Expected content type 'application/json'" ++
-             " found " ++ contentType))
-
-  let value = left (\s -> JSONDecodeException ("mmGetJSONBody: " ++ label ++ ": " ++ s)
-                                              (HTTP.rspBody rsp))
-                   (A.eitherDecode (BL.pack (HTTP.rspBody rsp)))
-  hoistE $ do
-    y <- value
-    return (y)
-
-doRequest :: HTTP.RequestMethod -> String -> B.ByteString -> Session -> IO HTTP.Response_String
-doRequest method uri payload (Session cd token) = do
-  path <- mmPath ("/api/v4" ++ uri)
-  rawResponse <- withConnection cd $ \con -> do
-    let contentLength = B.length payload
-        request = HTTP.Request
-          { HTTP.rqURI = path
-          , HTTP.rqMethod = method
-          , HTTP.rqHeaders =
-            [ HTTP.mkHeader HTTP.HdrAuthorization ("Bearer " ++ getTokenString token)
-            , HTTP.mkHeader HTTP.HdrHost          (T.unpack $ cdHostname cd)
-            , HTTP.mkHeader HTTP.HdrUserAgent     HTTP.defaultUserAgent
-            , HTTP.mkHeader HTTP.HdrContentType   "application/json"
-            , HTTP.mkHeader HTTP.HdrContentLength (show contentLength)
-            ] ++ autoCloseToHeader (cdAutoClose cd)
-          , HTTP.rqBody    = B.unpack payload
-          }
-    HTTP.simpleHTTP_ con request
-  rsp <- hoistE (left ConnectionException rawResponse)
-  case HTTP.rspCode rsp of
-    (2, _, _) -> return rsp
-    code -> do
-      case A.eitherDecode (BL.pack (HTTP.rspBody rsp)) of
-        Right err ->
-          throwIO (err :: MattermostError)
-        Left _ ->
-          throwIO (HTTPResponseException ("Server returned unexpected " ++ show code ++ " response"))
-
-
-mkQueryString :: [Maybe (String, String)] -> String
-mkQueryString ls =
-  List.intercalate "&" [ k ++ "=" ++ v | Just (k, v) <- ls ]
-
-jsonBody :: A.ToJSON i => i -> B.ByteString
-jsonBody = BL.toStrict . A.encode
-
-noBody :: B.ByteString
-noBody = B.empty
-
-
-inPost
-  :: String
-  -> B.ByteString
-  -> (HTTP.Response_String -> IO o)
-  -> Session
-  -> IO o
-inPost uri payload k session =
-  doRequest HTTP.POST uri payload session >>= k
-
-inPut
-  :: String
-  -> B.ByteString
-  -> (HTTP.Response_String -> IO o)
-  -> Session
-  -> IO o
-inPut uri payload k session =
-  doRequest HTTP.PUT uri payload session >>= k
-
-inGet
-  :: String
-  -> B.ByteString
-  -> (HTTP.Response_String -> IO o)
-  -> Session
-  -> IO o
-inGet uri payload k session =
-  doRequest HTTP.GET uri payload session >>= k
-
-inDelete
-  :: String
-  -> B.ByteString
-  -> (HTTP.Response_String -> IO o)
-  -> Session
-  -> IO o
-inDelete uri payload k session =
-  doRequest HTTP.DELETE uri payload session >>= k
-
-
-
-doUnauthRequest :: HTTP.RequestMethod -> String -> B.ByteString -> ConnectionData -> IO HTTP.Response_String
-doUnauthRequest method uri payload cd = do
-  path <- mmPath ("/api/v4" ++ uri)
-  rawResponse <- withConnection cd $ \con -> do
-    let contentLength = B.length payload
-        request = HTTP.Request
-          { HTTP.rqURI = path
-          , HTTP.rqMethod = method
-          , HTTP.rqHeaders =
-            [ HTTP.mkHeader HTTP.HdrHost          (T.unpack $ cdHostname cd)
-            , HTTP.mkHeader HTTP.HdrUserAgent     HTTP.defaultUserAgent
-            , HTTP.mkHeader HTTP.HdrContentType   "application/json"
-            , HTTP.mkHeader HTTP.HdrContentLength (show contentLength)
-            ] ++ autoCloseToHeader (cdAutoClose cd)
-          , HTTP.rqBody    = B.unpack payload
-          }
-    HTTP.simpleHTTP_ con request
-  rsp <- hoistE (left ConnectionException rawResponse)
-  case HTTP.rspCode rsp of
-    (2, _, _) -> return rsp
-    code -> throwIO (HTTPResponseException ("Server returned unexpected " ++ show code ++ " response"))
 
 mmLogin :: ConnectionData -> Login -> IO (Either LoginFailureException (Session, User))
 mmLogin cd login = do
@@ -201,65 +39,6 @@ mmInitialUser cd users = do
   case HTTP.rspCode rsp of
     (2, _, _) -> mmGetJSONBody "User" rsp
     _ -> error ("Server returned unexpected " ++ show (HTTP.rspCode rsp) ++ " response")
-
--- mmLogin :: ConnectionData -> Login -> IO (Either LoginFailureException (Session, User))
--- mmLogin cd login = do
---   let rawPath = "/api/v4/users/login"
---   path <- mmPath rawPath
---   rsp <- withConnection cd $ \ con -> do
---     let content = BL.toStrict (A.encode login)
---         contentLength = B.length content
---         request = HTTP.Request
---           { rqURI = path
---           , rqMethod = HTTP.POST
---           , rqHeaders = [ HTTP.mkHeader HTTP.HdrHost (T.unpack (cdHostname cd))
---                         , HTTP.mkHeader HTTP.HdrUserAgent HTTP.defaultUserAgent
---                         , HTTP.mkHeader HTTP.HdrContentType "application/json"
---                         , HTTP.mkHeader HTTP.HdrContentLength (show contentLength)
---                         ] ++ autoCloseToHeader (cdAutoClose cd)
---           , rqBody = B.unpack content
---           }
---     HTTP.simpleHTTP_ con request
---   rsp' <- hoistE (left ConnectionException rsp)
---   if HTTP.rspCode rsp' /= (2,0,0)
---     then let eMsg = "Server returned unexpected " ++ show (HTTP.rspCode rsp') ++ " response"
---          in return (Left (LoginFailureException eMsg))
---     else do
---       token <- mmGetHeader rsp' (HTTP.HdrCustom "Token")
---       value <- mmGetJSONBody "User" rsp'
---       return (Right (Session cd (Token token), value))
-
-
--- NECESSARY ENDPOINTS
--- , mmLogin
--- , mmGetAllChannelsForUser
--- , mmGetAllChannelDataForUser
--- , mmGetAllChannelsWithDataForUser
--- , mmGetMoreChannels
--- , mmGetPosts
--- , mmGetPostsSince
--- , mmGetPostsBefore
--- , mmGetPostsAfter
--- , mmGetReactionsForPost
--- , mmGetFile
--- , mmGetUsers
--- , mmGetTeamMembers
--- , mmGetChannelMembers
--- , mmGetProfilesForDMList
--- , mmGetMe
--- , mmGetProfiles
--- , mmGetStatuses
--- , mmGetInitialLoad
--- , mmSaveConfig
--- , mmSetChannelHeader
--- , mmTeamAddUser
--- , mmUsersCreate
--- , mmUsersCreateWithSession
--- , mmPost
--- , mmGetConfig
--- , mmFlagPost
--- , mmUnflagPost
--- , mkPendingPost
 
 -- * Endpoints
 
--- src/Network/Mattermost/Util.hs
+++ src/Network/Mattermost/Util.hs
@@ -23,8 +23,11 @@ import           Network.Connection ( Connection
                                     , ConnectionParams(..)
                                     , TLSSettings(..)
                                     , connectionGet
+                                    , connectionGetLine
+                                    , connectionPut
                                     , connectionClose
                                     , connectTo )
+import qualified Network.HTTP.Stream as HTTP
 
 import           Network.Mattermost.Types.Internal
 
@@ -59,11 +62,26 @@ dropTrailingChar _ = ""
 
 -- | Creates a new connection to 'Hostname' from an already initialized 'ConnectionContext'.
 -- Internally it uses 'bracket' to cleanup the connection.
-withConnection :: ConnectionData -> (Connection -> IO a) -> IO a
+withConnection :: ConnectionData -> (MMConn -> IO a) -> IO a
 withConnection cd action =
-  bracket (mkConnection cd)
-          connectionClose
+  bracket (MMConn <$> mkConnection cd)
+          (connectionClose . fromMMConn)
           action
+
+maxLineLength :: Int
+maxLineLength = 2^(16::Int)
+
+newtype MMConn = MMConn { fromMMConn :: Connection }
+
+-- | This instance allows us to use 'simpleHTTP' from 'Network.HTTP.Stream' with
+-- connections from the 'connection' package.
+instance HTTP.Stream MMConn where
+  readLine   con       = Right . B.unpack . dropTrailingChar <$> connectionGetLine maxLineLength (fromMMConn con)
+  readBlock  con n     = Right . B.unpack <$> connectionGetExact (fromMMConn con) n
+  writeBlock con block = Right <$> connectionPut (fromMMConn con) (B.pack block)
+  close      con       = connectionClose (fromMMConn con)
+  closeOnEnd _   _     = return ()
+
 
 -- | Creates a connection from a 'ConnectionData' value, returning it. It
 --   is the user's responsibility to close this appropriately.
