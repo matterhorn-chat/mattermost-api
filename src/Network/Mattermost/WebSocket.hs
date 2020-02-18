209a1e8ee3dd5436ac1afc0f3d739e23596091c7
Isaiah Mindich <isaiah@galois.com>
2020-02-18 09:11:06 -0800

    Use cdDirectory field when making requests and websocket connections

--- src/Network/Mattermost/WebSocket.hs
+++ src/Network/Mattermost/WebSocket.hs
@@ -164,7 +164,7 @@ mmWithWebSocket (Session cd (Token tk)) recv body = do
         body (MMWS c health) `catch` propagate [mId, pId]
   WS.runClientWithStream stream
                       (T.unpack $ cdHostname cd)
-                      "/api/v4/websocket"
+                      (cdDirectory cd ++ "/api/v4/websocket")
                       WS.defaultConnectionOptions { WS.connectionOnPong = onPong }
                       [ ("Authorization", "Bearer " <> B.pack tk) ]
                       action
